4df99bb3f95412b55e6cdd89d5875c27da7f2a16
Dorian Ignee <dorian.ignee@posterselect.de>
2023-05-03 09:01:59 +0200

    Vorschläge von daCypher

--- src/Zusatzaufgaben_Programmieren1/Eratosthenes.java
+++ src/Zusatzaufgaben_Programmieren1/Eratosthenes.java
@@ -1,50 +1,63 @@
 package Zusatzaufgaben_Programmieren1;            // Meine Lösung
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Eratosthenes {
-
-        public static int oberGrenze;
-
     //a
-    public static int[] fillArray(int oberGrenze) {
-        int[] arg1 = new int[oberGrenze];
-
-        for (int i = 0; i < arg1.length; i++) {
-            arg1[i] = i + 1;
-        }
-        return arg1;
+    public static int[] fillArray(int upperBound) { // Wenn alles auf englisch ist, dann benenne deine Variablen am Besten auch englisch
+        var result = new int[upperBound + 1]; // Ich hab hier mal die 0 auch als natürliche Zahl aufgenommen. Weiß nicht, ob das ok ist.
+        Arrays.setAll(result, id -> id);
+        return result;
     }
 
     // b
-    public static int[] sieve(int[] arg2) {
+    public static int[] sieve(int[] array) {
 
-        int grenze = (int) Math.sqrt(oberGrenze);
-        arg2[0] = -1;
+        int maxTest = (int) Math.sqrt(array.length);
+        array[0] = -1; // 0 und 1 sind per Definition keine Primzahlen
+        array[1] = -1;
 
-        for (int i = 2; i <= grenze; i++) {
-            for (int j = 2; j < oberGrenze; j++)
-                if (arg2[j] % i == 0) arg2[j] = -1;
+        for (int i = 2; i <= maxTest; i++) {
+            if (array[i] == -1) continue; // Wenn die Zahl schon als Primzahl erkannt wurde, soll sie nicht als Faktor für den Test benutzt werden
+            for (int j = 2*i; j < array.length; j+=i) // Mit der Schrittweite (j+=i) nehmen wir automatisch nur die Vielfachen von i mit
+                array[j] = -1;
         }
 
-        return arg2;
+        return array;
     }
 
     // c
-    public static void showContent(int[] arg3){
-
-        System.out.println("0 is not a prime number");
-
-        for (int i = 0; i < oberGrenze; i++) {
-        if(arg3[i] == -1) System.out.println(i + 1 + " is not a prime number");
-        else System.out.println(arg3[i] + " is a prime number");
-        }
+    public static void showContent(int[] array){
+        for (int i = 0; i<array.length; i++)
+            System.out.printf(
+                "%d is %sa prime number%n", 
+                i, 
+                array[i]==-1 ? "not ": ""
+            );
     }
-        public static void main(String[] args) {
+
+    public static void showPrimesOnly(int[] array) {
+        System.out.println(
+            Arrays
+                .stream(array)
+                .filter(n -> n > 0)
+                .mapToObj(Integer::toString)
+                .collect(Collectors.joining(", "))
+        );
+    }
+
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Test numbers: ");
-        oberGrenze = sc.nextInt();
-        showContent(sieve(fillArray(oberGrenze)));              // 3 Methoden ineinander verschachtelt
+        int upperBound = sc.nextInt();
+
+        var array = sieve(fillArray(upperBound));
+        showContent(array);
+
+        System.out.println("\nPrimes only:");
+        showPrimesOnly(array);  // Testweise nur die Primzahlen anzeigen (ist übersichtlicher)
+        sc.close();
     }
 }
